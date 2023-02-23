65f00f83282737a645a41b807e8ff569293a6132
denis.legin <denis.legin@gs-labs.ru>
2023-02-24 02:13:17 +0300

    - saving and listening data created

--- glucosereader/app/src/main/java/com/codehunters/glucosereader/HostViewModel.kt
+++ glucosereader/app/src/main/java/com/codehunters/glucosereader/HostViewModel.kt
@@ -3,36 +3,48 @@ package com.codehunters.glucosereader
 import android.nfc.Tag
 import android.nfc.tech.NfcA
 import androidx.lifecycle.ViewModel
-import com.codehunters.presenter.ITransceiveObject
+import androidx.lifecycle.viewModelScope
+import com.codehunters.data.models.GlucoseData
+import com.codehunters.presenter.interfaces.IGlucosePresenter
+import com.codehunters.presenter.interfaces.ITransceiveObject
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HostViewModel @Inject constructor(
-    private val transceiveObject: ITransceiveObject
+    private val transceiveObject: ITransceiveObject,
+    private val glucosePresenter: IGlucosePresenter
 ) : ViewModel() {
 
-    fun getNfcData(tag: Tag?) {
+    fun getNfcData(tag: Tag?): List<ByteArray> {
+        var data: List<ByteArray>
         NfcA.get(tag).apply {
-            this.connect()
-            this.sendData()
-            this.close()
+            connect()
+            data = sendData()
+            close()
         }
+        return data
     }
 
-    private fun NfcA.sendData(): List<ByteArray?> {
-        return transceiveObject.getData().map {
-            var isNotReceived = true
-            var receivedInfo = byteArrayOf()
-            while (isNotReceived) {
-                runCatching {
-                    transceive(it)
-                }.getOrNull()?.let {
-                    isNotReceived = false
-                    receivedInfo = it
-                }
+    private fun NfcA.sendData(): List<ByteArray> {
+        val receivedInfo = mutableListOf<ByteArray>()
+        transceiveObject.getData().map {
+            runCatching {
+                transceive(it)
+            }.getOrNull()?.let {
+                receivedInfo.add(it)
             }
-            receivedInfo
+        }
+        return receivedInfo
+    }
+
+    private fun saveData(value: Float) {
+        viewModelScope.launch(Dispatchers.IO) {
+            glucosePresenter.add(
+                GlucoseData(value = value, creationDate = System.currentTimeMillis())
+            )
         }
     }
 }
--- glucosereader/app/src/main/java/com/codehunters/glucosereader/ui/main/HomeViewModel.kt
+++ glucosereader/app/src/main/java/com/codehunters/glucosereader/ui/main/HomeViewModel.kt
@@ -1,7 +1,31 @@
 package com.codehunters.glucosereader.ui.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.codehunters.data.models.GlucoseData
+import com.codehunters.presenter.interfaces.IGlucosePresenter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class HomeViewModel @Inject constructor(
+    private val glucosePresenter: IGlucosePresenter
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow<List<GlucoseData>>(emptyList())
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        renderGlucose()
+    }
+
+    private fun renderGlucose() {
+        viewModelScope.launch(Dispatchers.IO) {
+            glucosePresenter.getAllFlow().collect {
+                _uiState.value = it
+            }
+        }
+    }
 }
